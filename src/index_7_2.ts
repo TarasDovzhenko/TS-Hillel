c442c286f4661113defa5aa710802e752ef4c760
TarasDovzhenko <tarasdovzhenkoua@gmail.com>
2025-01-17 16:33:09 +0200

    7.2 fix

--- src/index_7_2.ts
+++ src/index_7_2.ts
@@ -7,9 +7,19 @@ function fetchData(): unknown {
     return { name: "Alice", age: 25 };
 }
 
-function printPersonInfo(person: { name: string; age: number }): void {
-    console.log(`Name: ${person.name}, Age: ${person.age}`);
+function printPersonInfo(person: unknown): asserts person is Person {
+    if (
+        typeof person !== "object" ||
+        person === null ||
+        typeof (person as any).name !== "string" ||
+        typeof (person as any).age !== "number"
+    ) {
+        throw new Error("Error");
+    }
 }
 
-const data = fetchData() as Person;
+const data = fetchData();
+
 printPersonInfo(data);
+
+console.log(`Name: ${(data as Person).name}, Age: ${(data as Person).age}`);
