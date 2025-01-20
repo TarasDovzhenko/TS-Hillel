43695490e58242d5a4236133554cc7f4a8e997c0
TarasDovzhenko <tarasdovzhenkoua@gmail.com>
2025-01-20 18:38:10 +0200

    8 fix

--- dist/index_7_2.js
+++ dist/index_7_2.js
@@ -1,9 +1,15 @@
 "use strict";
 function fetchData() {
-    return { name: "Alice", age: 25 };
+    return { name: "Tara", age: 25 };
 }
 function printPersonInfo(person) {
-    console.log(`Name: ${person.name}, Age: ${person.age}`);
+    if (typeof person !== "object" ||
+        person === null ||
+        typeof person.name !== "string" ||
+        typeof person.age !== "number") {
+        throw new Error("Error");
+    }
 }
 const data = fetchData();
 printPersonInfo(data);
+console.log(`Name: ${data.name}, Age: ${data.age}`);
